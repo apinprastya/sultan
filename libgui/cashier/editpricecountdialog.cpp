15e8b9d125d47a741041f7003d1acca4b8830e5f
Apin <apin.klas@gmail.com>
2017-11-17 06:10:08 +0700

    add missing copyright on header

--- libgui/cashier/editpricecountdialog.cpp
+++ libgui/cashier/editpricecountdialog.cpp
@@ -1,3 +1,22 @@
+/*
+ * editpricecountdialog.cpp
+ * Copyright 2017 - ~, Apin <apin.klas@gmail.com>
+ *
+ * This file is part of Sultan.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "editpricecountdialog.h"
 #include "ui_editpricecountdialog.h"
 #include "global_constant.h"
