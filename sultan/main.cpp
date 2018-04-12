ae84922a278388bb70c185e24cb898aee5245712
Apin <apin.klas@gmail.com>
2018-04-12 19:57:35 +0700

    update version to 18.04.01

--- sultan/main.cpp
+++ sultan/main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 #endif
     a.setApplicationName("sultan");
     a.setOrganizationName("lekapin");
-    a.setApplicationVersion("17.12.01");
+    a.setApplicationVersion("18.04.01");
 
 #ifndef QT_DEBUG
     QDir dir = QDir::home();
